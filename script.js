577990348f08784f5cae1e1eabeae441884c11d1
Adam-Kai-dev <93033963+Adam-kai-dev@users.noreply.github.com>
2023-08-04 15:38:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+// define all UI variable
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".site-navbar ul");
+const navLinks = document.querySelectorAll(".site-navbar a");
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener("click", togglerClick);
+  // nav links click event
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
+  }
+}
+
+// Add Cutom Play Button to Video
+const video = document.getElementById("video");
+const circlePlayButton = document.getElementById("circle-play-b");
+
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+circlePlayButton.addEventListener("click", togglePlay);
+video.addEventListener("playing", function () {
+  circlePlayButton.style.opacity = 0;
+});
+video.addEventListener("pause", function () {
+  circlePlayButton.style.opacity = 1;
+});
+
+// Slider
+const slider = document.querySelector("[data-slider]");
+
+const track = slider.querySelector("[data-slider-track]");
+const prev = slider.querySelector("[data-slider-prev]");
+const next = slider.querySelector("[data-slider-next]");
+
+if (track) {
+  prev.addEventListener("click", () => {
+    next.removeAttribute("disabled");
+
+    track.scrollTo({
+      left: track.scrollLeft - track.firstElementChild.offsetWidth,
+      behavior: "smooth",
+    });
+  });
+
+  next.addEventListener("click", () => {
+    prev.removeAttribute("disabled");
+
+    track.scrollTo({
+      left: track.scrollLeft + track.firstElementChild.offsetWidth,
+      behavior: "smooth",
+    });
+  });
+
+  track.addEventListener("scroll", () => {
+    const trackScrollWidth = track.scrollWidth;
+    const trackOuterWidth = track.clientWidth;
+
+    prev.removeAttribute("disabled");
+    next.removeAttribute("disabled");
+
+    if (track.scrollLeft <= 0) {
+      prev.setAttribute("disabled", "");
+    }
+
+    if (track.scrollLeft === trackScrollWidth - trackOuterWidth) {
+      next.setAttribute("disabled", "");
+    }
+  });
+}
+
+// Start Hero Section 2
+const navbarMenu = document.getElementById("menu");
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    if (navbarMenu.classList.contains("is-active")) {
+      navbarMenu.classList.remove("is-active");
+    }
+  }
+});
+
+// Show image when hove on it in single product
+function changeimg(url, e) {
+  document.getElementById("img").src = url;
+  let nodes = document.getElementById("thumb_img");
+  let img_child = nodes.children;
+  for (i = 0; i < img_child.length; i++) {
+    img_child[i].classList.remove("active");
+  }
+  e.classList.add("active");
+}
+
+// singleProInfo
+// Quantity
+function increaseCount(a, b) {
+  var input = b.previousElementSibling;
+  var value = parseInt(input.value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  input.value = value;
+}
+
+function decreaseCount(a, b) {
+  var input = b.nextElementSibling;
+  var value = parseInt(input.value, 10);
+  if (value > 1) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    input.value = value;
+  }
+}
+
+// Start Slider Price
+// var prSlider = document.getElementById("myRange");
+// var output = document.getElementById("price");
+// output.innerHTML = prSlider.value;
+
+// prSlider.oninput = function () {
+//   output.innerHTML = this.value;
+// };
